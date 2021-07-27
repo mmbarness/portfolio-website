ceeffc434e71c2a3d45027e7705642732bf2e1cb
matthew barnes <011mbarnes@gmail.com>
2021-07-27 17:45:11 -0400

    mhm

--- frontend/components/top-bar.jsx
+++ frontend/components/top-bar.jsx
@@ -4,7 +4,7 @@ import '../style/top-bar.scss'
 export const TopBar = () => {
     return(
         <div className="top-bar-container">
-            <p>Matthew Barnes</p>
+            <p id="my-name">Matthew Barnes</p>
             <ul id="project-link-list">
                 <li id="fullstack-li">
                     <a href="http://record-collector.matthewbarnes.tech/#/" target="_blank">
