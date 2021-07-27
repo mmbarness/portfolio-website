b0aae36b0bd3989abc72786bb3e8bc50a9b4bc74
matthew barnes <011mbarnes@gmail.com>
2021-07-27 17:06:03 -0400

    all links open in new tab

--- dist/bundle.js
+++ dist/bundle.js
@@ -222,17 +222,23 @@ var Home = function Home(props) {
     classname: "bio-block-text",
     id: "bio-block-body-1"
   }, "A year ago, I had just returned to my job as a bartender at ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-    href: "https://www.newyorker.com/magazine/2020/11/02/pandemic-essentials-of-all-kinds-at-hunky-dory"
+    href: "https://www.newyorker.com/magazine/2020/11/02/pandemic-essentials-of-all-kinds-at-hunky-dory",
+    target: "_blank"
   }, "Hunky Dory"), ", a restaurant/bar in Crown Heights, and was happy to be back. However, the three or so months of that first lockdown had already given me enough time to realize it wasn\u2019t what I wanted to be doing for much longer. Spurred on by my peers, I gave programming more and more serious consideration; a year later I\u2019m now conversant in React ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-    href: "https://github.com/mmbarness/recordcollector/blob/main/frontend/components/artists/artist_show.jsx"
+    href: "https://github.com/mmbarness/recordcollector/blob/main/frontend/components/artists/artist_show.jsx",
+    target: "_blank"
   }, "hooks"), ", ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-    href: "https://github.com/mmbarness/home_court/blob/8c12270de2833b434b07ce916bcd5baf54a74e78/frontend/src/components/session/address_form_field.jsx"
+    href: "https://github.com/mmbarness/home_court/blob/8c12270de2833b434b07ce916bcd5baf54a74e78/frontend/src/components/session/address_form_field.jsx",
+    target: "_blank"
   }, "typeahead search"), ", and CSS techniques like ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-    href: "https://github.com/mmbarness/call-sheet/blob/master/src/styles/infoModal.css"
+    href: "https://github.com/mmbarness/call-sheet/blob/master/src/styles/infoModal.css",
+    target: "_blank"
   }, "flexbox"), ", ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-    href: "https://github.com/mmbarness/recordcollector/blob/7d0140a66ca25b992a58a63110f5e1d5961dd906/app/assets/stylesheets/artist/artist-show-page.scss#L54"
+    href: "https://github.com/mmbarness/recordcollector/blob/7d0140a66ca25b992a58a63110f5e1d5961dd906/app/assets/stylesheets/artist/artist-show-page.scss#L54",
+    target: "_blank"
   }, "grid"), ", and ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
-    href: "https://github.com/mmbarness/call-sheet/blob/e433233e7c767e3ad40606197896ddcbc240c054/src/styles/topBar/search.scss#L37"
+    href: "https://github.com/mmbarness/call-sheet/blob/e433233e7c767e3ad40606197896ddcbc240c054/src/styles/topBar/search.scss#L37",
+    target: "_blank"
   }, "animations"), ".")), (0,_email_me__WEBPACK_IMPORTED_MODULE_3__.EmailMe)());
 };
 
